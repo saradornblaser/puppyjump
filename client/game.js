7cd681554828dd7e964202eb990e89b387eaafb4
Sara Dornblaser <saradornblaser@gmail.com>
2019-01-12 17:24:30 -0500

    refactoring

--- client/game.js
+++ client/game.js
@@ -2,8 +2,6 @@ import Puppy from './Puppy'
 const spriteSheet = new Image();
 spriteSheet.src = '/sprites.png'; // this is the URL (from client perspective)
 
-
-
 let jumpingPuppy = new Puppy(42, 14, spriteSheet, 2, 10);
 console.log(jumpingPuppy)
 
